4caeacbfd173590acf76c4dd842c21b09de32976
clementcode <mrdominiqueclement@gmail.com>
2022-03-01 11:28:11 +0100

    indent all

--- gestionTemps.js
+++ gestionTemps.js
@@ -1,17 +1,26 @@
-const joursSemaine = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"]
+/** @format */
 
+const joursSemaine = [
+  "lundi",
+  "mardi",
+  "mercredi",
+  "jeudi",
+  "vendredi",
+  "samedi",
+  "dimanche",
+];
 
-let ajd = new Date()
-let options = { weekday: "long" }
-let jourActuel = ajd.toLocaleDateString("fr-FR", options)
+let ajd = new Date();
+let options = { weekday: "long" };
+let jourActuel = ajd.toLocaleDateString("fr-FR", options);
 // console.log(jourActuel,ajd);
 
-jourActuel = jourActuel.charAt(0).toUpperCase() + jourActuel.slice(1)
+jourActuel = jourActuel.charAt(0).toUpperCase() + jourActuel.slice(1);
 
-
-let tabJoursEnOrdre = joursSemaine.slice(joursSemaine.indexOf(jourActuel)).concat(joursSemaine.slice(0, joursSemaine.indexOf(jourActuel)))
+let tabJoursEnOrdre = joursSemaine
+  .slice(joursSemaine.indexOf(jourActuel))
+  .concat(joursSemaine.slice(0, joursSemaine.indexOf(jourActuel)));
 
 console.log(tabJoursEnOrdre);
 
-
-export default tabJoursEnOrdre
+export default tabJoursEnOrdre;
